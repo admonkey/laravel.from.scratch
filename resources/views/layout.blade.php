109b9b2c728ef4b33482c0d79e504f7096acb2ab
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-30 15:16:36 -0500

    create sass css with elixer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
         <title>Laravel</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/css/app.css">
         @yield('header')
     </head>
     <body>
