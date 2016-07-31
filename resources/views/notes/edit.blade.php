dcd7f7fe6b1f2c734cf0193c8349a2ef7c189718
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-31 15:22:41 -0500

    create form on edit Note view

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -7,7 +7,18 @@
 
       <h1>Edit Note {{ $note->id }}</h1>
 
+      <form method="POST" action="">
 
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+        <div class="form-group">
+          <textarea name="body" class="form-control">{{ $note->body }}</textarea>
+        </div>
+
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary">Update Note</button>
+        </div>
+      </form>
 
     </div>
   </div>
