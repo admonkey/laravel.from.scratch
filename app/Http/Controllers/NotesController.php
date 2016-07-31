bfe84ef95d86ee23dc6287e9d3a0842a504048fe
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-31 12:27:45 -0500

    handle create note form submission

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Card;
+use App\Note;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class NotesController extends Controller
 {
-  public function store(Request $request)
+  public function store(Request $request, Card $card)
   {
-    return $request->all();
+//     return $card;
+    $note = new Note;
+    $note->body = $request->body;
+    $card->notes()->save($note);
+    return back();
   }
 }
