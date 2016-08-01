fae15b70205e31beb5c76ae925c7647df8ce8648
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-31 22:18:22 -0500

    add jquery bootstrap top nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,12 @@
 
     <h1>Welcome Page</h1>
 
+    <p class="lead">
+      <a target="_blank" href="https://laracasts.com/series/laravel-5-from-scratch/">
+        Original Tutorial https://laracasts.com/series/laravel-5-from-scratch/
+      </a>
+    </p>
+
     @if (empty($people))
       There are no people.
     @else
