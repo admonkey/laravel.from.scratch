1a366f04b70674f0b9ede1eddb4a66f9498b3615
Jeff Puckett <jeff@jeffpuckett.com>
2016-08-04 22:16:20 -0500

    use session() helper

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -1,3 +1,3 @@
-@if (Session::has('flash_message'))
-  <div class="alert alert-{{ Session::get('flash_message_level') }}">{{ Session::get('flash_message') }}</div>
+@if (session()->has('flash_message'))
+  <div class="alert alert-{{ session('flash_message_level') }}">{{ session('flash_message') }}</div>
 @endif
